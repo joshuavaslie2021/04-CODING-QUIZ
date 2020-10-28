3456fdeaa65c6b8c240908db83c063b5cf10bbb4
joshuavaslie2021 <joshuavaslie2021@u.northwestern.edu>
2020-10-27 23:10:31 -0500

    functional timer added, text style editing

--- Assets/logic.js
+++ Assets/logic.js
@@ -3,13 +3,31 @@ var startButton = document.getElementById("start")
 var choicesEl = document.getElementById("choices")
 var questionEl = document.getElementById("questions")
 var feedbackEl = document.getElementById("feedback")
+var openingEl = document.getElementById("start-screen")
+var userScore = 0
 
 function startQuiz() {
+openingEl.style.visibility = 'hidden'
  for (var i = 0; i < questions.length; i++)   {
 
  }
  // Timer code
+ startTimer()
  getQuestion();
+}
+
+function startTimer() {
+var timer = setInterval(startTime,1000);
+var start = 75
+
+function startTime() {
+    document.getElementById("time").innerHTML = -- start;
+    if (start === 0) {
+        clearInterval(timer)
+        alert("time is up!")
+        }
+    }    
+  
 }
 
 function getQuestion() {
@@ -35,11 +53,16 @@ function answerClick() {
     }
     else {
         feedbackEl.textContent = "Correct!"
+        userScore++
     } 
     currentQuestionIndex++
     
     if (currentQuestionIndex === questions.length) {
-        console.log("game over")   
+        questionEl.style.visibility = "hidden"
+        
+        feedbackEl.textContent = "you scored " + userScore + " out of " + questions.length + "  correctly"
+        
+      
     }
     else {
  
